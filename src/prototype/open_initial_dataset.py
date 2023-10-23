b4b082ee0ee079d079bb576dd787aa54cb344b0e
Timothy Baldock <2385679b@student.gla.ac.uk>
2023-10-23 17:42:18 +0100

    explored data set

---
+++ src/prototype/open_initial_dataset.py
@@ -0,0 +1,31 @@
+import gzip
+import os
+import jsonlines
+from tqdm import tqdm
+import pickle
+
+current_directory = os.getcwd()
+parent_directory = os.path.split(current_directory)[0]
+parent_directory = os.path.split(parent_directory)[0]
+file_path = os.path.join(parent_directory, 'data', 'raw', 'mesh_1410000.jsonl.gz')
+pickle_file_path = os.path.join(parent_directory, 'data', 'raw', 'first_million_articles.pkl')
+
+with gzip.open(file_path,'rt') as f:
+
+    reader = jsonlines.Reader(f)
+    first_1000000_articles = []
+
+    for i in tqdm(range(1000000)):
+        article = reader.read()
+        article_key_info = {}
+        article_key_info["pmid"] = article["pmid"]
+        article_key_info["date"] = article["publication_date"]
+        mesh_names = []
+        for mesh_tag in article["mesh"]:
+            mesh_names.append(mesh_tag["name"])
+        article_key_info["mesh_names"] = mesh_names
+        first_1000000_articles.append(article_key_info)
+
+with open(pickle_file_path, 'wb') as fp:
+    pickle.dump(first_1000000_articles, fp)
+
--- src/prototype/test_initial_dataset.py
+++ src/prototype/test_initial_dataset.py
@@ -1,18 +1,28 @@
-import gzip
+import pickle
 import os
-import jsonlines
+from tqdm import tqdm
 
-current_directory = os.path.dirname(__file__)
+current_directory = os.getcwd()
 parent_directory = os.path.split(current_directory)[0]
 parent_directory = os.path.split(parent_directory)[0]
-file_path = os.path.join(parent_directory, 'data', 'raw', 'mesh_1410000.jsonl.gz')
+pickle_file_path = os.path.join(parent_directory, 'data', 'raw', 'first_million_articles.pkl')
 
-with gzip.open(file_path,'rt') as f:
+with open(pickle_file_path, 'rb') as fp:
+    articles = pickle.load(fp)
+    print(articles[0])
 
-    reader = jsonlines.Reader(f)
+years = {}
+articles_after_2007 = 0
+articles_before_2007 = 0
+for art in tqdm(articles):
+    if art["date"][0] > 2006:
+        articles_after_2007 += 1
+    else:
+        articles_before_2007 += 1
+    if art["date"][0] in years:
+        years[art["date"][0]] = years[art["date"][0]] + 1
+    else:
+        years[art["date"][0]] = 1
 
-    documents = [ doc for doc in reader ]
-
-len(documents)
-
- 
+print("articles_before_2007:", articles_before_2007)
+print("articles_after_2007:", articles_after_2007)
