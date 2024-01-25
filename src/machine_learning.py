149f15398c852f8857c82df53aa80072788161e4
Timothy Baldock <2385679b@student.gla.ac.uk>
2024-01-25 14:47:02 +0000

    fiddling with system

--- src/machine_learning.py
+++ src/machine_learning.py
@@ -25,6 +25,8 @@ pytorch_graph_before = from_networkx(before_2008_graph)
 pytorch_graph_after = from_networkx(after_2008_graph)
 print("converted graphs")
 
+pytorch_graph_before.node_id = pytorch_graph_before.edge_index[0].unique()
+
 transform = T.RandomLinkSplit(
     num_val = 0.49,
     num_test = 0.49,
@@ -34,15 +36,58 @@ transform = T.RandomLinkSplit(
 train_graph, validate_graph, test_graph = transform(pytorch_graph_after)
 print("split validate, test data")
 
-training_load = LinkNeighborLoader(
-    data = pytorch_graph_before,
-    batch_size = 128,
-    num_neighbors = [20,10],
-    shuffle = True,
-)
-print("Defined batch loader")
+class GNN(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super().__init__()
+        self.conv1 = SAGEConv(hidden_channels, hidden_channels)
+        self.conv2 = SAGEConv(hidden_channels, hidden_channels)
 
+    def forward(self, x, edge_index):
+        print(x)
+        x = F.relu(self.conv1(x, edge_index))
+        x = self.conv2(x, edge_index)
+        return x
 
-print("======================================")
-print(pytorch_graph_before.edge_index)
-print(pytorch_graph_before.num_edges)
+class Classifier(torch.nn.Module):
+    def forward(self, x_emb, x_lin):
+        return (x_emb * x_lin).sum(dim=-1)
+
+class Model(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super().__init__()
+        self.emb = torch.nn.Embedding(pytorch_graph_before.num_nodes, hidden_channels)
+        self.gnn = GNN(hidden_channels)
+        self.classifier = Classifier()
+    
+    def forward(self, data):
+        x_dict = {
+          "emb": self.emb(data.node_id),
+          "lin": self.emb(data.node_id),
+        } 
+        x_dict = self.gnn(x_dict, data.edge_index)
+        pred = self.classifier(
+            x_dict["emb"],
+            x_dict["lin"],
+        )
+        return pred
+
+model = Model(hidden_channels=64)
+print("define model")
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f"Device: '{device}'")
+model = model.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+for epoch in range(1, 6):
+    total_loss = total_examples = 0
+    optimizer.zero_grad()
+    pytorch_graph_before.to(device)
+    pred = model(pytorch_graph_before)
+    ground_truth = test_graph
+    loss = F.binary_cross_entropy_with_logits(pred, ground_truth)
+    loss.backward()
+    optimizer.step()
+    total_loss += float(loss) * pred.numel()
+    total_examples += pred.numel()
+    print(f"Epoch: {epoch:03d}, Loss: {total_loss / total_examples:.4f}")
+print("training complete")
--- src/prototype/medium_link_prediction_example.py
+++ src/prototype/medium_link_prediction_example.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.metrics import roc_auc_score
 import tqdm
 import torch
-from torch_geometric.data import download_url, extract_zip, HeteroData
+from torch_geometric.data import download_url, extract_zip, HeteroData, Data
 import torch_geometric.transforms as T
 from torch_geometric.loader import LinkNeighborLoader
 from torch_geometric.nn import SAGEConv, to_hetero
@@ -70,7 +70,15 @@ data["user", "rates", "movie"].edge_index = edge_index_user_to_movie
 # in order to let a GNN be able to pass messages in both directions.
 # We can leverage the `T.ToUndirected()` transform for this from PyG:
 data = T.ToUndirected()(data)
-print("prepare graph")
+print("prepared graph")
+
+new_data = Data()
+new_data.x = torch.stack([ratings_user_id, ratings_movie_id])
+new_data.edge_index = edge_index_user_to_movie
+number_of_nodes = len(unique_user_id) + len(movies_df)
+new_data.node_id = torch.arange(number_of_nodes)
+new_data.x = movie_feat
+new_data = T.ToUndirected()(new_data)
 
 # For this, we first split the set of edges into
 # training (80%), validation (10%), and testing edges (10%).
@@ -84,11 +92,20 @@ transform = T.RandomLinkSplit(
     num_test=0.1,
     disjoint_train_ratio=0.3,
     neg_sampling_ratio=2.0,
-    add_negative_train_samples=False,
+    add_negative_train_samples=False, 
     edge_types=("user", "rates", "movie"),
     rev_edge_types=("movie", "rev_rates", "user"), 
 )
 train_data, val_data, test_data = transform(data)
+
+new_transform = T.RandomLinkSplit(
+    num_val=0.1,
+    num_test=0.1,
+    disjoint_train_ratio=0.3,
+    neg_sampling_ratio=2.0,
+    add_negative_train_samples=False, 
+)
+new_train_data, new_val_data, new_test_data = transform(new_data)
 print("define data split")
 
 # In the first hop, we sample at most 20 neighbors.
@@ -115,35 +132,39 @@ class GNN(torch.nn.Module):
         super().__init__()
         self.conv1 = SAGEConv(hidden_channels, hidden_channels)
         self.conv2 = SAGEConv(hidden_channels, hidden_channels)
-    def forward(self, x:torch.Tensor , edge_index: torch.Tensor):
-
+    def forward(self, x, edge_index):
+        print(x)
         x = F.relu(self.conv1(x, edge_index))
         x = self.conv2(x, edge_index)
         return x
 # Our final classifier applies the dot-product between source and destination
 # node embeddings to derive edge-level predictions:
 class Classifier(torch.nn.Module):
-    def forward(self, x_user: torch.Tensor, x_movie: torch.Tensor, edge_label_index: torch.Tensor):
+    def forward(self, x_user, x_movie, edge_label_index):
         # Convert node embeddings to edge-level representations:
         edge_feat_user = x_user[edge_label_index[0]]
         edge_feat_movie = x_movie[edge_label_index[1]]
         # Apply dot-product to get a prediction per supervision edge:
         return (edge_feat_user * edge_feat_movie).sum(dim=-1)
+    
+class New_Classifier(torch.nn.Module):
+    def forward(self, x_emb, x_lin):
+        # Convert node embeddings to edge-level representations:
+        # Apply dot-product to get a prediction per supervision edge:
+        return (x_emb * x_lin).sum(dim=-1)
 
 class Model(torch.nn.Module):
     def __init__(self, hidden_channels):
         super().__init__()
         # Since the dataset does not come with rich features, we also learn two
         # embedding matrices for users and movies:
-        self.movie_lin = torch.nn.Linear(20, hidden_channels)
-        self.user_emb = torch.nn.Embedding(data["user"].num_nodes, hidden_channels)
-        self.movie_emb = torch.nn.Embedding(data["movie"].num_nodes, hidden_channels)
+        self.lin = torch.nn.Linear(20, hidden_channels)
+        self.emb = torch.nn.Embedding(new_data.num_nodes, hidden_channels)
+        
         # Instantiate homogeneous GNN:
         self.gnn = GNN(hidden_channels)
-        # Convert GNN model into a heterogeneous variant:
-        self.gnn = to_hetero(self.gnn, metadata=data.metadata())
         self.classifier = Classifier()
-    def forward(self, data: HeteroData):
+    def forward(self, data):
         x_dict = {
           "user": self.user_emb(data["user"].node_id),
           "movie": self.movie_lin(data["movie"].x) + self.movie_emb(data["movie"].node_id),
@@ -167,21 +188,20 @@ model = model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 for epoch in range(1, 6):
     total_loss = total_examples = 0
-    for sampled_data in tqdm.tqdm(train_loader):
-        optimizer.zero_grad()
-        sampled_data.to(device)
-        pred = model(sampled_data)
-        ground_truth = sampled_data["user", "rates", "movie"].edge_label
-        loss = F.binary_cross_entropy_with_logits(pred, ground_truth)
-        loss.backward()
-        optimizer.step()
-        total_loss += float(loss) * pred.numel()
-        total_examples += pred.numel()
+    optimizer.zero_grad()
+    train_data.to(device)
+    pred = model(train_data)
+    ground_truth = train_data["user", "rates", "movie"].edge_label
+    loss = F.binary_cross_entropy_with_logits(pred, ground_truth)
+    loss.backward()
+    optimizer.step()
+    total_loss += float(loss) * pred.numel()
+    total_examples += pred.numel()
     print(f"Epoch: {epoch:03d}, Loss: {total_loss / total_examples:.4f}")
 print("training complete")
 
 # Define the validation seed edges:
-edge_label_index = val_data["user", "rates", "movie"].edge_label_index
+edge_label_index = new_val_data["user", "rates", "movie"].edge_label_index
 edge_label = val_data["user", "rates", "movie"].edge_label
 val_loader = LinkNeighborLoader(
     data=val_data,
@@ -191,15 +211,13 @@ val_loader = LinkNeighborLoader(
     batch_size=3 * 128,
     shuffle=False,
 )
-sampled_data = next(iter(val_loader))
 
 preds = []
 ground_truths = []
-for sampled_data in tqdm.tqdm(val_loader):
-    with torch.no_grad():
-        sampled_data.to(device)
-        preds.append(model(sampled_data))
-        ground_truths.append(sampled_data["user", "rates", "movie"].edge_label)
+with torch.no_grad():
+    val_data.to(device)
+    preds.append(model(val_data))
+    ground_truths.append(val_data.edge_index)
 pred = torch.cat(preds, dim=0).cpu().numpy()
 ground_truth = torch.cat(ground_truths, dim=0).cpu().numpy()
 auc = roc_auc_score(ground_truth, pred)
